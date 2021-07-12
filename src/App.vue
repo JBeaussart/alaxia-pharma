f969cadc29275af7066abb452ee5aec1b985047c
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-07-12 23:46:31 +0200

    primitive design navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <Navbar />
-  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
@@ -13,12 +12,20 @@ import Navbar from './components/Navbar.vue';
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  width: 70%;
+  margin: 0 auto;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,72 +1,48 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-  >
-    <el-menu-item index="1">Centre de traitement</el-menu-item>
-    <el-submenu index="2">
-      <template #title>Lieu de travail</template>
-      <el-menu-item index="2-1">item un</el-menu-item>
-      <el-menu-item index="2-2">item deux</el-menu-item>
-      <el-menu-item index="2-3">item trois</el-menu-item>
-      <el-submenu index="2-4">
-        <template #title>item quatre</template>
-        <el-menu-item index="2-4-1">item un</el-menu-item>
-        <el-menu-item index="2-4-2">item deux</el-menu-item>
-        <el-menu-item index="2-4-3">item trois</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>Infos</el-menu-item>
-    <el-menu-item index="4"
-      ><a href="https://www.ele.me" target="_blank">Commandes</a></el-menu-item
-    >
-  </el-menu>
-  <div class="line"></div>
-  <el-menu
-    :default-active="activeIndex2"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
-  >
-    <el-menu-item index="1">Centre de traitement</el-menu-item>
-    <el-submenu index="2">
-      <template #title>Lieu de travail</template>
-      <el-menu-item index="2-1">item un</el-menu-item>
-      <el-menu-item index="2-2">item deux</el-menu-item>
-      <el-menu-item index="2-3">item trois</el-menu-item>
-      <el-submenu index="2-4">
-        <template #title>item quatre</template>
-        <el-menu-item index="2-4-1">item un</el-menu-item>
-        <el-menu-item index="2-4-2">item deux</el-menu-item>
-        <el-menu-item index="2-4-3">item trois</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4"
-      ><a href="https://www.ele.me" target="_blank">Commandes</a></el-menu-item
-    >
-  </el-menu>
+  <nav class="container">
+    <ul>
+      <li>ABOUT</li>
+      <li>ALX-009</li>
+      <li>CYSTIC FIBROSIS</li>
+      <li>PARTNERS</li>
+      <li>SCIENTIFIC BOARD</li>
+      <li>NEWS</li>
+      <li>EVENTS</li>
+      <li>POSTERS AND TALKS</li>
+      <li>CONTACT</li>
+      <li><img class="logo" src="../assets/logo.jpeg" alt="logo alaxia" /></li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      activeIndex: '1',
-      activeIndex2: '1',
-    };
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
-    },
-  },
-};
+export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+nav {
+  height: 60px;
+  line-height: 60px;
+  margin-bottom: 40px;
+  ul {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+    border-bottom: 2px solid #025293;
+    li {
+      font-size: 10px;
+      color: #025293;
+      font-weight: bold;
+      cursor: pointer;
+    }
+    li:hover {
+      color: #3bb8e3;
+    }
+  }
+}
+.logo {
+  height: 50px;
+  padding-top: 20px;
+}
+</style>
