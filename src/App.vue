3fe0ab24ec75813d622b87b06ff1df43e7156197
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-07-14 15:40:44 +0200

    change the default font from Avenir to Lato

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,17 @@ import Footer from './components/Footer.vue';
 </script>
 
 <style>
+/* IMPORT FONT FAMILY LATO FROM GOOGLE FONT */
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+
+/* GENERAL SETTINGS */
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Lato, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
