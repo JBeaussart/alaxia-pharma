a70b387c7c65b982c3e11b3931aae39b146d1c43
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-07-14 15:37:28 +0200

    create the footer component file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Navbar />
   <router-view></router-view>
+  <Footer />
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer_container">
+    <div class="container">
+      <h3>WARNING</h3>
+      <p>
+        Please be aware that some people are fraudulently using our name and
+        logo to offer fake jobs without our consent. We at Alaxia never ask job
+        seekers to pay or purchase anything while applying for a job, nor cash
+        in any check. Please do not respond to such proposals. Be sure that we
+        are doing everything we can to stop these fraudulent actions. Only jobs
+        offered and/or discussed with Alaxia French Team are real.
+      </p>
+      <h3>KEEP IN TOUCH</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.footer_container {
+  background-color: rgb(220, 220, 220);
+}
+</style>
