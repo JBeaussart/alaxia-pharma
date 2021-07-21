1c386e3ebfb775b7e584a6a1438c749216d20159
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-07-21 09:14:34 -0400

    add the banner for alx page

--- src/pages/ALX009.vue
+++ src/pages/ALX009.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <h1>ALX009</h1>
+  <div class="alx_banner">
+    <div class="alx_banner_content container">
+      <h2>ALX009</h2>
+      <p>
+        ALX-009 is a first-in class orphan drug candidate for Cystic Fibrosis
+        (CF) patients. It consists in the combination of two endogenous
+        substances, hypothiocyanite (OSCN-) and lactoferrin, exhibiting
+        antimicrobial properties compensating the defective innate immune system
+        in CF patients. ALX-009 is presented as a solution for inhalation for
+        the symptomatic treatment of lung infections.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -8,4 +18,17 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.alx_banner {
+  text-align: left;
+  height: 250px;
+  margin-top: -40px;
+  background-color: #025293;
+  padding-top: 36px;
+  padding-bottom: 36px;
+  h2,
+  p {
+    color: white;
+  }
+}
+</style>
