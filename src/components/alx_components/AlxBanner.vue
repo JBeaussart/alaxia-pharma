876039808b30259594794738c0fbf261c5b16acf
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-07-22 17:46:12 -0400

    create component for each element in the alx page

---
+++ src/components/alx_components/AlxBanner.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="alx_banner">
+    <div class="alx_banner_content container">
+      <h2 class="alx_banner_content_title">ALX009</h2>
+      <br />
+
+      <p class="alx_banner_content_description">
+        ALX-009 is a first-in class orphan drug candidate for Cystic Fibrosis
+        (CF) patients. <br /><br />
+        It consists in the combination of two endogenous substances,
+        hypothiocyanite (OSCN-) and lactoferrin, exhibiting antimicrobial
+        properties compensating the defective innate immune system in CF
+        patients. ALX-009 is presented as a solution for inhalation for the
+        symptomatic treatment of lung infections.
+      </p>
+    </div>
+    <div class="btn container">
+      <p>
+        <a href="#overview"
+          ><i class="fas fa-binoculars btn_icon"></i> Overview</a
+        >
+      </p>
+      <p>
+        <a href="#efficacy"><i class="fas fa-bolt btn_icon"></i> Efficacy</a>
+      </p>
+      <p>
+        <a href="#further_reading"
+          ><i class="fas fa-book-open btn_icon"></i> Further reading</a
+        >
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  text-align: justify;
+}
+.alx_banner {
+  position: relative;
+  height: 250px;
+  margin-top: -40px;
+  background-color: #025293;
+  padding-top: 36px;
+  padding-bottom: 36px;
+  h2,
+  p {
+    color: white;
+  }
+}
+.alx_banner_content_title {
+  font-size: 36px;
+}
+.alx_banner_content_description {
+  text-align: justify;
+  font-size: 14px;
+}
+.btn {
+  position: absolute;
+  bottom: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 0 auto;
+  margin-bottom: 8px;
+}
+a {
+  text-decoration: none;
+  cursor: pointer;
+  opacity: 0.8;
+  background: white;
+  color: #025293;
+  margin-right: 12px;
+  border: 1px solid white;
+  border-radius: 4px;
+  padding: 8px;
+  transition: 0.3s all ease-in-out;
+  &:hover {
+    opacity: 0.6;
+  }
+}
+.btn_icon {
+  transform: translateY(2px);
+  font-size: 16px;
+}
+</style>
---
+++ src/components/alx_components/Efficacy.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="efficacy" class="container efficacy">
+    <h2 class="alx_part_title">
+      <i class="fas fa-bolt banner_icon"></i>
+      EFFICACY
+    </h2>
+    <br />
+    <section>
+      <h3>
+        ALX-009 (OSCN-/bLF) – a therapeutic option to fight broader bacterial
+        infections
+      </h3>
+      <br />
+      <div>
+        <p class="alx_page_schema_description">
+          Tests performed by Alaxia or in collaboration with Queen’s University
+          Belfast or Colorado State University
+        </p>
+        <img
+          class="alx_page_schema"
+          src="../../assets/alx_page/alx_page__efficacy_first_schemajpg.png"
+          alt=""
+        />
+      </div>
+    </section>
+
+    <br />
+    <br />
+
+    <section>
+      <h3>
+        Time killing profile of ALX-009 against emergent Cystic Fibrosis
+        pathogens or Selected agent
+      </h3>
+      <br />
+      <p class="alx_page_schema_description">
+        Achromobacter spp, Pseudomonas aeruginosa, Burkholderia cepacia complex,
+        Stenotrophomonas maltophilia, Burkholderia pseudomallei
+      </p>
+      <div class="img_efficacy">
+        <img
+          class="alx_page_schema_small"
+          src="../../assets/alx_page/alx_page__efficacy_second_schemajpg.jpg"
+          alt=""
+        />
+        <img
+          class="alx_page_schema_small"
+          src="../../assets/alx_page/alx_page__efficacy_third_schemajpg.jpg"
+          alt=""
+        />
+      </div>
+    </section>
+    <br />
+    <br />
+    <section>
+      <h3>ALX-009 (OSCN-/bLF) – bactericidal effect in biological matrices</h3>
+      <br />
+      <p class="alx_page_schema_description">
+        In contrast with antibiotics, ALX-009 activity is not altered by complex
+        structures such as biofilm and/or sputum that are present in lung
+        infections
+      </p>
+      <div class="img_efficacy">
+        <img
+          class="alx_page_schema_small"
+          src="../../assets/alx_page/alx_page__efficacy_fourth_schemajpg.jpg"
+          alt=""
+        />
+        <img
+          class="alx_page_schema_small"
+          src="../../assets/alx_page/alx_page__efficacy_six_schemajpg.jpg"
+          alt=""
+        />
+      </div>
+      <p>
+        ALX-009 kills bacteria embedded in P. aeruginosa biofilms at
+        concentrations similar to planktonic cultures. Presence of bLF allows
+        decreasing OSCN- dose
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.alx_page_second_schema {
+  width: 60vw;
+  margin: 0 auto;
+}
+.img_efficacy {
+  display: flex;
+  justify-content: space-around;
+}
+.container {
+  text-align: justify;
+}
+.alx_part_title {
+  font-size: 24px;
+}
+.alx_page_schema {
+  width: 40vw;
+}
+.alx_page_schema_small {
+  width: 30vw;
+}
+.alx_page_schema_description {
+  font-style: italic;
+}
+</style>
