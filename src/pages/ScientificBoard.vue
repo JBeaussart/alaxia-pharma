0e500f7d284a7f4bf148b5d846bc82bad5a628c1
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-08-18 13:04:17 +0200

    remove useless code in scientific board

--- src/pages/ScientificBoard.vue
+++ src/pages/ScientificBoard.vue
@@ -23,32 +23,4 @@ h1 {
   font-size: 42px;
   margin-bottom: 40px;
 }
-// CARD DESIGN
-.card_id {
-  margin-bottom: 24px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.card_id_text_work {
-  font-size: 10px;
-}
-.card_id_text_name {
-  font-size: 16px;
-  text-decoration: underline;
-  text-underline-offset: 2px;
-}
-.card_id_text_post {
-  font-size: 10px;
-  font-style: italic;
-  margin-bottom: 4px;
-}
-.card_id_avatar_img {
-  width: 120px;
-}
-hr {
-  margin-top: 30px;
-  margin-bottom: 20px;
-  border-top: 1px solid #025293;
-}
 </style>
