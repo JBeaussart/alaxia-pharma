ad70b792500d4d4805883b206868775da6cc8366
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-08-19 18:31:09 +0200

    add google maps to contact page, create a component Maps

---
+++ src/components/Contact/Maps.vue
@@ -0,0 +1,26 @@
+<template>
+  <GoogleMap
+    api-key="AIzaSyBJzzExmr51QOrYFjeB85104Zpr8rMvEzI"
+    style="width: 100%; height: 400px"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 45.73395341922623, lng: 4.876272021661882 };
+
+    return { center };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    <h1>Contact</h1>
+  <div class="maps">
+    <Maps />
   </div>
 </template>
 
 <script>
-export default {};
+import Maps from "../components/Contact/Maps.vue";
+export default {
+  components: {
+    Maps,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.maps {
+  margin-top: -40px;
+}
+</style>
